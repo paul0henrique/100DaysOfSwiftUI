c3f7abc7b8dd5f88eb994cc2bedd83f3ed0e9f06
Paulo Henrique <paulohenriquesms10@gmail.com>
2024-06-10 21:35:34 -0300

    Day 9 part 2

--- 100DaysOfSwiftUI/Days/Day9.swift
+++ 100DaysOfSwiftUI/Days/Day9.swift
@@ -8,14 +8,14 @@
 import Foundation
 struct Day9: Day {
     static func main() {
-
+        
         func greetUser() {
             print("Hi, there")
         }
         
         greetUser()
         
-        var greetCopy = greetUser
+        let greetCopy = greetUser
         greetCopy()
         
         let sayHello = {
@@ -40,7 +40,7 @@ struct Day9: Day {
         let user = data(1989)
         print(user)
         
-        let team = ["Paulo", "Bernardo", "Gabi", "João", "David"]
+        let team = ["Paulo", "Bernardo", "Gabi", "João", "David", "Teste"]
         let sortedTeam = team.sorted()
         print(sortedTeam)
         
@@ -50,11 +50,42 @@ struct Day9: Day {
             } else if name2 == "Paulo" {
                 return false
             }
-
+            
             return name1 < name2
         })
         
         print(captainFirstTeam)
         
+        let payment = { (user: String) -> Bool in
+            print("Paying \(user)...")
+            return true
+        }
+        
+        let newPayment = { () -> Bool in
+            print("Paying an anonymous person...")
+            return true
+        }
+        
+        print(newPayment())
+        
+        let reverseTeam = team.sorted {
+            return $0 > $1
+        }
+        
+        let tOnly = team.filter { $0.hasPrefix("T") }
+        print(tOnly)
+        
+        let uppercaseTeam = team.map { $0.uppercased() }
+        print(uppercaseTeam)
+        
+        // Trailling closures
+        func animate(duration: Double, animations: () -> Void){
+            print("Starting a \(duration) second animation...")
+            animations()
+        }
+        
+        animate(duration: 3){
+            print("Fade out the image")
+        }
     }
 }
