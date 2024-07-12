ae5d5a1e50ab0776d4060e5b9a8c186a5dad02ce
Paulo <paulohenriquesms10@gmail.com>
2024-07-11 21:13:18 -0300

    day 10

--- 100DaysOfSwiftUI/Days/Day10.swift
+++ 100DaysOfSwiftUI/Days/Day10.swift
@@ -45,5 +45,51 @@ struct Day10: Day {
         var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
         archer.takeVacation(days: 5)
         print(archer.vacationRemaining)
+        
+        struct Game {
+            var score = 0 {
+                didSet {
+                    print("Score is now \(score)")
+                }
+            }
+        }
+        
+        var game = Game()
+        game.score += 10
+        game.score -= 3
+        game.score += 1
+        
+        struct App {
+            var contacts = [String]() {
+                willSet {
+                    print("Current value is: \(contacts)")
+                    print("New value will be: \(newValue)")
+                }
+                
+                didSet {
+                    print("There are now \(contacts.count) contacts.")
+                    print("Old value was \(oldValue)")
+                }
+            }
+        }
+        
+        var app = App()
+        app.contacts.append("Paulo")
+        app.contacts.append("Gabi")
+        app.contacts.append("Bernardo")
+        
+        struct Player {
+            let name: String
+            let number: Int
+            
+            init(name: String) {
+                self.name = name
+                number = Int.random(in: 1...9)
+            }
+        }
+        
+        let player = Player(name: "Megan R")
+        print(player.name)
     }
+    
 }
